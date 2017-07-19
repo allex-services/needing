fb65f1268b300ebed5858100b0cbca5f378e6215
Veljko Popovic <v.s.popovic@gmail.com>
2017-07-19 16:37:50 +0200

    updating dependencies

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function createServicePack(execlib){
 
   return {
     service: {
-      dependencies: ['allex:servicecollection']
+      dependencies: ['allex_servicecollectionservice']
     },
     sinkmap: {
-      dependencies: ['allex:servicecollection']
+      dependencies: ['allex_servicecollectionservice']
     },
     tasks: {
       dependencies: []
