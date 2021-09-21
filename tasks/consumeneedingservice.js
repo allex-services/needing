4a07d121cf83d7210731c5c091aa95fec156373d
Andrija Petrovic <andrija.hers@gmail.com>
2021-09-21 14:28:57 +0200

    A-syncing the NeedingServiceConsumer

--- tasks/consumeneedingservice.js
+++ tasks/consumeneedingservice.js
@@ -2,6 +2,7 @@ function createConsumeNeedingService(execlib){
   'use strict';
   var lib = execlib.lib,
       q = lib.q,
+      qlib = lib.qlib,
       execSuite = execlib.execSuite,
       SinkTask = execSuite.SinkTask,
       taskRegistry = execSuite.taskRegistry;
@@ -55,7 +56,6 @@ function createConsumeNeedingService(execlib){
     });
   };
   NeedingServiceConsumer.prototype.serveNeeds = function(){
-    var nl, i, need;
     if(!this.needs){
       return;
     }
@@ -64,23 +64,55 @@ function createConsumeNeedingService(execlib){
       this.log('Cannot start serving needs at all');
       return;
     }
+    this.biddingCycle();
+  };
+  NeedingServiceConsumer.prototype.biddingCycle = function () {
+    this.findBiddableNeed().done(this.onBiddableNeed.bind(this)); //findBiddableNeed must not reject
+  };
+  NeedingServiceConsumer.prototype.findBiddableNeed = function (index) {
+    if (!this.needs) {
+      return q(null);
+    }
+    var nl, need;
+    index = lib.isNumber(index) ? index : 0;
     nl = this.needs.length;
+    if (index>nl) {
+      return q(null);
+    }
     if(nl>this.startIndex){
-      for (i=this.startIndex; i<nl; i++) {
-        need = this.needs[i];
-        if (this.isNeedBiddable(need)) {
-          break;
-        }
-        need = null;
-      }
-      if(!need){
-        this.log('No more needs to bid on');
-        return;
-      }
-      this.produceNeed(need);
+      need = this.needs[index];
+      var ret = qlib.thenableRead(this.isNeedBiddable(need)).then(
+        this.onNeedBiddable.bind(this, need, index),
+        this.onNeedBiddableFail
+      );
+      need = null;
+      index = null;
+      return ret;
     }else{
       this.log('No more needs');
+      return q(null);
     }
+  };
+  NeedingServiceConsumer.prototype.onNeedBiddable = function (need, index, isbid) {
+    if (isbid) {
+      var ret = need;
+      need = null;
+      return ret;
+    }
+    return this.onNeedBiddableFail(index, null);
+  };
+  NeedingServiceConsumer.prototype.onNeedBiddableFail = function (index, reason_ignored) {
+    var ret = this.findBiddableNeed(index+1);
+    index = null;
+    return ret;
+  };
+  NeedingServiceConsumer.prototype.onBiddableNeed = function (need) {
+    if(!need){
+      this.log('No more needs to bid on');
+      return;
+    }
+    this.produceNeed(need);
+    this.biddingCycle();
   };
   NeedingServiceConsumer.prototype.produceNeed = function(need){
     var d = q.defer();
@@ -105,14 +137,7 @@ function createConsumeNeedingService(execlib){
     });
   };
   NeedingServiceConsumer.prototype.isNeedBiddable = function(need){
-    var ssn = this.shouldServeNeed(need);
-    if(ssn){
-      if('function' === typeof ssn.done){
-        ssn.done(this.serveNeeds.bind(this));
-        return false;
-      }
-      return true;
-    }
+    return this.shouldServeNeed(need);
   };
   NeedingServiceConsumer.prototype.onChallenge = function(need,challenge,defer){
     this.respondToChallenge(need,challenge,defer);
